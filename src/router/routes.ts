f984a4df37ac651a974a322cc902568cccf8e11c
Tim Middleton <x@vaults.ca>
2022-07-23 11:45:11 -0400

    fix to allow no path

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,15 +1,19 @@
 import { RouteRecordRaw } from 'vue-router';
-import { MODE } from 'src/lib/modes'
+import { MODE } from 'src/lib/modes';
 
-const mode_name_str = Object.values(MODE).map(e => e.name_stripped.slice(0,3)+'[^/]*').join('|')
+const mode_name_str = Object.values(MODE)
+  .map((e) => e.name_stripped.slice(0, 3) + '[^/]*')
+  .join('|');
 
 const routes: RouteRecordRaw[] = [
   {
-    path: `/:mode(${mode_name_str})/:die*`,
+    path: `/:mode(${mode_name_str})*/:die*`,
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') },
-               {path: 'test', component: () => import('pages/TestPage.vue')}],
-},
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'test', component: () => import('pages/TestPage.vue') },
+    ],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
